3bdbd8bd4aa326b1591d4d9a279e674d6dd8b837
pink_pika <pink_pika@cmoney.net.tw>
2022-02-17 18:04:03 +0800

    fix: fix demo viewcontroller

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -8,37 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var version1: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("APIRequestDesignVersion1 call apis", for: .normal)
+        button.addAction(UIAction(handler: {
+            action in
+            APIManager.shared.requestHttpBinGetByURLSession {
+                result in print(result)
+            }
+            APIManager.shared.requestHttpBinGetByAlamofire {
+                result in print(result)
+            }
+            APIManager.shared.requestHttpBinPost {
+                result in print(result)
+            }
+        }), for: .touchUpInside)
+        return button
+    }()
+    
+    var version2: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("APIRequestDesignVersion2 call apis", for: .normal)
+        button.addAction(UIAction(handler: {
+            action in
+        }), for: .touchUpInside)
+        return button
+    }()
+    
+    var version3: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("APIRequestDesignVersion3 call apis", for: .normal)
+        button.addAction(UIAction(handler: {
+            action in
+        }), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        APIManager.shared.requestHttpBinGetByURLSession {
-            result in
-            switch result{
-            case .success(let response):
-                print(response)
-            case .failure(let error):
-                print(error)
-            }
-        }
-        
-        APIManager.shared.requestHttpBinGetByAlamofire {
-            result in
-            switch result{
-            case .success(let response):
-                print(response)
-            case .failure(let error):
-                print(error)
-            }
-        }
-        
-        APIManager.shared.requestHttpBinPost {
-            result in
-            switch result{
-            case .success(let response):
-                print(response)
-            case .failure(let error):
-                print(error)
-            }
-        }
+        version1.frame = CGRect(x: 0, y: 100, width: 300, height: 50)
+        view.addSubview(version1)
+        version2.frame = CGRect(x: 0, y: 150, width: 300, height: 50)
+        view.addSubview(version2)
+        version3.frame = CGRect(x: 0, y: 200, width: 300, height: 50)
+        view.addSubview(version3)
     }
 }
